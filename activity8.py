202219fb53a910b8d2aee086f9fb6dd8204aa4ee
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2019-03-04 16:31:54 -0800

    Added mean computation, iterations, helper functions

--- activity8.py
+++ activity8.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn import svm
 
 def main(argv, argc):
-    if (argc < 1):
+    if (argc < 2):
         print("Usage: python main.py <dataset>")
         return 1
 
@@ -27,23 +27,66 @@ def find_centroid(features):
 
 def k_means_cluster(cluster_amount, features):
     (centroid_1, centroid_2) = find_centroid(features)
+    print("Initial randomly selected centroids:")
+    print("- Centroid 1:\t(" + str(centroid_1[0]) + ", " + str(centroid_1[1]) + ")")
+    print("- Centroid 2:\t(" + str(centroid_2[0]) + ", " + str(centroid_2[1]) + ")")
+
+    centroid_1_group = []
+    centroid_2_group = []
+
+    iterations = 10
+    for iteration in range(0, iterations):
+        centroid_1_group, centroid_2_group = compute_closeness(centroid_1, centroid_2, features)
+        og_centroid_1 = centroid_1
+        og_centroid_2 = centroid_2
+
+        centroid_1 = compute_centroid_means(centroid_1_group)
+        centroid_2 = compute_centroid_means(centroid_2_group)
+        print("\nResults for Iteration " + str(iteration + 1) + ":")
+        print("Group 1 (with centroid (" + str(og_centroid_1[0]) + ", " + str(og_centroid_1[1]) + "))" + ":")
+        print_cluster(centroid_1_group)
+        print("Group 2 (with centroid (" + str(og_centroid_2[0]) + ", " + str(og_centroid_2[1]) + "))" + ":")
+        print_cluster(centroid_2_group)
+        print("- New Centroid 1: (" + str(centroid_1[0]) + ", " + str(centroid_1[1]) + ")")
+        print("- New Centroid 2: (" + str(centroid_2[0]) + ", " + str(centroid_2[1]) + ")")
+
+
+def print_cluster(cluster):
+    for c in cluster:
+        print("\t(" + str(c[0]) + ", " + str(c[1]) + ")")
+
+
+# I had no idea what to call this function
+def compute_closeness(centroid_1, centroid_2, features):
     centroid_1_group, centroid_2_group = [], []
     for i in features:
-# compute distance formula for both centroids
-        v1 = (centroid_1[0]-(i[0]))**2
-        v2 = (centroid_1[1]-(i[1]))**2
+        # compute distance formula for both centroids
+        v1 = (centroid_1[0] - (i[0])) ** 2
+        v2 = (centroid_1[1] - (i[1])) ** 2
         c1 = math.sqrt(v1 + v2)
-        
-        v1 = (centroid_2[0]-(i[0]))**2
-        v2 = (centroid_2[1]-(i[1]))**2
+
+        v1 = (centroid_2[0] - (i[0])) ** 2
+        v2 = (centroid_2[1] - (i[1])) ** 2
         c2 = math.sqrt(v1 + v2)
-        
+
         if c1 > c2:
             centroid_1_group.append(i)
         else:
             centroid_2_group.append(i)
+    return centroid_1_group, centroid_2_group
 
-# recompute location of centroids by finding their means by their respective lists
+
+def compute_centroid_means(centroid_group):
+    # recompute location of centroids by finding their means by their respective lists
+
+    xSum = 0.0
+    ySum = 0.0
+    count = float(len(centroid_group))
+    for cg in centroid_group:
+        xSum += cg[0]
+        ySum += cg[1]
+    return (xSum / count), (ySum / count)
+
 
 
 def get_data(file):
